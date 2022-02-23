87ac133cbddbe1852fe5b73d52a1584a4fc8de0a
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-02-23 16:07:21 +0100

    Add 28.3 - TASK - Websocket - Chat Application - websockets added

--- client/app.js
+++ client/app.js
@@ -1,3 +1,6 @@
+const socket = io();
+socket.on('message', ({ author, content }) => addMessage(author, content))
+
 const data = {
   loginForm: document.getElementById('welcome-form'),
   messagesSection: document.getElementById('messages-section'),
@@ -18,35 +21,45 @@ function login(event) {
     userName = data.userNameInput.value;
     data.loginForm.classList.remove('show');
     data.messagesSection.classList.add('show');
-    console.log(userName);
+    // console.log(userName);
+    socket.emit('join', userName);
   } else {
     alert('What is your name?');
   }
 }
 
-function sendMessage(event) {
-  event.preventDefault();
-
+function sendMessage(e) {
+  e.preventDefault();
   let messageContent = data.messageContentInput.value;
-  if (messageContent) {
+
+  if(!messageContent.length) {
+    alert('Sending nothing is little stupid...');
+  }
+  else {
     addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent })
     data.messageContentInput.value = '';
-  } else {
-    alert('Sending nothing is little stupid...')
   }
 };
 
 function addMessage(author, content) {
+  const message = document.createElement('li');
 
-  const message = document.createElement('list');
   message.classList.add('message');
+
   message.classList.add('message--received');
-  if (author === userName) message.classList.add('message--self');
+  if(author === userName) {
+    message.classList.add('message--self'); 
+  } else if (author === 'Chat Bot') {
+    message.classList.add('message--bot');
+  }
+
   message.innerHTML = `
-    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
     <div class="message__content">
       ${content}
     </div>
   `;
+  
   data.messagesList.appendChild(message);
 }
